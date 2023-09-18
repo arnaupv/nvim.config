e4ba3e616d9497947b283dd1699ccca3df1ed591
Arnau Pont <arnau.pont@paxport.net>
2023-09-18 19:02:40 +0200

    Getting rid of unncessary LSP config (default config in LazyVim)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,119 +1,11 @@
 return {
-  -- mason
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        -- Javascript
-        -- "eslint_d",
-        -- "deno",
-        -- xml
-        "lemminx",
-        -- Python
-        "pyright",
-        -- Docker
-        "dockerfile-language-server",
-        "docker-compose-language-service",
-      },
-    },
-  },
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        -- https://alpha2phi.medium.com/modern-neovim-debugging-and-testing-8deda1da1411
-        --
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                typeCheckingMode = "off",
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "workspace",
-              },
-            },
-          },
-        },
-      },
-      autoformat = false,
-      -- setup = {
-      --   pyright = function(_, opts)
-      --     local lsp_utils = require("plugins.lsp.utils")
-      --     lsp_utils.on_attach(function(client, buffer)
-      --         -- stylua: ignore
-      --         if client.name == "pyright" then
-      --           vim.keymap.set("n", "<leader>tC", function() require("dap-python").test_class() end, { buffer = buffer, desc = "Debug Class" })
-      --           vim.keymap.set("n", "<leader>tM", function() require("dap-python").test_method() end, { buffer = buffer, desc = "Debug Method" })
-      --           vim.keymap.set("v", "<leader>tS", function() require("dap-python").debug_selection() end, { buffer = buffer, desc = "Debug Selection" })
-      --         end
-      --     end)
-      --   end,
-      -- },
+      autoformat = true,
     },
   },
-  -- nav-buddy
-  {
-    "SmiteshP/nvim-navbuddy",
-    -- event = "LspAttach",
-    dependencies = {
-      { "neovim/nvim-lspconfig" },
-      { "SmiteshP/nvim-navic" },
-      { "MunifTanjim/nui.nvim" },
-      { "numToStr/Comment.nvim" },
-    },
-    opts = {
-      window = {
-        size = "80%",
-        border = "rounded",
-        sections = {
-          left = {
-            size = "20%",
-            border = nil, -- You can set border style for each section individually as well.
-          },
-          mid = {
-            size = "60%",
-            border = nil,
-          },
-          right = {
-            -- No size option for right most section. It fills to
-            -- remaining area.
-            border = nil,
-            preview = "leaf", -- Right section can show previews too.
-            -- Options: "leaf", "always" or "never"
-          },
-        },
-      },
-      lsp = { auto_attach = true },
-    },
-  },
-  -- nav-navic
-  {
-    "SmiteshP/nvim-navic",
-    event = "LspAttach",
-    dependencies = {
-      { "neovim/nvim-lspconfig" },
-    },
-  },
-  -- mason-null-ls
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "bufreadpre", "bufnewfile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "jose-elias-alvarez/null-ls.nvim",
-    },
-    opts = {
-      ensure_installed = {},
-      automatic_setup = false,
-      automatic_installation = true,
-      -- handlers = {},
-    },
-  },
-  -- custom language specific extension modules
   { import = "plugins.lang.java" },
   { import = "plugins.lang.markdown" },
   { import = "plugins.lang.lua" },
@@ -121,16 +13,4 @@ return {
   { import = "plugins.lang.python" },
   { import = "plugins.lang.docker" },
   { import = "plugins.lang.xml" },
-  -- null-ls
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    config = function()
-      local nls = require("null-ls")
-      nls.setup({
-        debounce = 150,
-        save_after_format = false,
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", ".git"),
-      })
-    end,
-  },
 }
