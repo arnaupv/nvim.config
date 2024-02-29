84563ef9d489315f5fa1482c8d9f6c158b8a22e3
Arnau Pont <arnau.pont@paxport.net>
2024-02-29 18:54:31 +0100

    No need of having java plugins as I'm not working in Java

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.java" },
+    -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.editor.leap" },
