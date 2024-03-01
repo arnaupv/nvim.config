399475b91f935ba32bb2480ae93329d779b3fca5
Arnau Pont <arnau.pont@paxport.net>
2024-03-01 13:26:26 +0100

    grouping docker functionalities in "cd"

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    plugins = { spelling = true },
+    defaults = {
+      mode = { "n", "v" },
+      ["<leader>cd"] = { name = "+docker" },
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
+}
