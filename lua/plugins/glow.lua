631b14d6bd1d8819eef25293b0b2b9ca3a6d823e
Arnau Pont <arnau.pont@paxport.net>
2024-04-15 16:19:24 +0200

    adding glow to visualize beautiful READMEs

---
+++ lua/plugins/glow.lua
@@ -0,0 +1,15 @@
+return {
+  "ellisonleao/glow.nvim",
+  config = true,
+  cmd = "Glow",
+  keys = {
+        -- stylua: ignore
+        {
+            "<leader>cg",
+            "<cmd>Glow<CR>",
+            desc = "Glow",
+            noremap = true,
+            silent = true,
+        },
+  },
+}
